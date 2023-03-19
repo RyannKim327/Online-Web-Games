784d0a964c8cd2e9a32aec5f183b5febd1007378
RyannKim327 <rksesgundo123@gmail.com>
2023-03-19 15:36:36 +0800

    03-19-23 15:36

--- games/jack-n-poy.js
+++ games/jack-n-poy.js
@@ -3,12 +3,17 @@ let paper = new elements("#jack-n-poy-paper")
 let scissor = new elements("#jack-n-poy-scissor")
 
 let enemy = new elements("#enemy")
-
+let choice = ""
+let canClick = true
 rock.onclick = () => {
-	send("rock")
+	if(canClick){
+		choice = "rock"
+		send()
+		canClick = false
+	}
 }
 
-async function send(choice){
+async function send(){
 	let json = {
 		choice
 	}
@@ -22,6 +27,28 @@ async function send(choice){
 		return response.json()
 	}).then(result => {
 		console.log(result)
+		if(result.enemy != ""){
+			switch(result.enemy){
+				case "rock":
+					enemy.innerHTML = '<i style="transform: rotate(90.0deg);" class="fa-solid fa-hand-back-fist"></i>'
+				break
+				case "paper":
+					enemy.innerHTML = '<i style="transform: rotate(90.0deg);" class="fa-solid fa-hand"></i>'
+				break
+				case "scissor":
+					enemy.innerHTML = '<i style="transform: scaleX(-1);" class="fa-solid fa-hand-scissors"></i>'
+				break
+				default:
+					enemy.innerHTML = "No Pic"
+			}
+			enemy.style.display = "inline"
+			setTimeout(() => {
+				canClick = false
+				enemy.style.display = "none"
+				enemy.innerHTML = ""
+				choice = ""
+			}, 5000)
+		}
 	}).catch(error => {
 		console.error(`Error [Fetch Server]: ${error}`)
 		setTimeout(send, 1500);
